8f800ba36e0a0898e7d0cc137cf5e73249fab515
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-05-11 21:34:01 +0200

    Arreglando los iconos de social

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+'''
 # Blogroll
 LINKS = (('Hacklab Almeria', 'http://hacklabalmeria.net/'),
          ('Pymiento Project', 'http://thepymientoproject.com/'),)
@@ -27,9 +27,20 @@ LINKS = (('Hacklab Almeria', 'http://hacklabalmeria.net/'),
 SOCIAL = (('Twitter', 'https://twitter.com/crisconru'),
           ('Github', 'https://github.com/crisconru/'),
           ('Linkedin', 'https://www.linkedin.com/in/cristobalcontrerasrubio'),)
-
+'''
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 THEME = 'Flex'
+
+# Flex configuration
+SITETITLE = "Crisconru's Home"
+SITESUBTITLE = "Bienvenido al rincon de Klin"
+SITELOGO = '/res/klins.png'
+MAIN_MENU = True
+LINKS = (('Hacklab Almeria', 'http://hacklabalmeria.net/'),
+         ('Pymiento Project', 'http://thepymientoproject.com/'),)
+SOCIAL = (('twitter', 'https://twitter.com/crisconru'),
+          ('github', 'https://github.com/crisconru/'),
+          ('linkedin', 'https://www.linkedin.com/in/cristobalcontrerasrubio'),)
