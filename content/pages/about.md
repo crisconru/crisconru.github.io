28d9727981644586171a1ebf96c73fd1be5e39d4
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-05-13 21:25:28 +0200

    Empezando el Sobre mi

---
+++ content/pages/about.md
@@ -0,0 +1,7 @@
+Author: Cristóbal Contreras  
+Title: Sobre mi  
+Date: 2018-05-13  
+Modified: 2018-05-13  
+
+Hola,  
+mi nombre Cristóbal Contreras Rubio, y como diría **Pepe Mújica** *"Soy del sur, vengo del sur."*
--- pelicanconf.py
+++ pelicanconf.py
@@ -18,20 +18,11 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-'''
-# Blogroll
-LINKS = (('Hacklab Almeria', 'http://hacklabalmeria.net/'),
-         ('Pymiento Project', 'http://thepymientoproject.com/'),)
-
-# Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/crisconru'),
-          ('Github', 'https://github.com/crisconru/'),
-          ('Linkedin', 'https://www.linkedin.com/in/cristobalcontrerasrubio'),)
-'''
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
 THEME = 'Flex'
 
 # Flex configuration
